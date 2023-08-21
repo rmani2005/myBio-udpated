dce803e4276150f732dd5526e915bea31ba98b52
Manikandan Ravi <rmani2005@gmail.com>
2023-08-21 16:02:18 +0000

    Inital copy after MongoDB connected, installed mongodb depedency and connected and fetched movies conetent

--- src/pages/api/mongodb.js
+++ src/pages/api/mongodb.js
@@ -15,8 +15,8 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Add Mongo URI to .env.local')
 }
 
-if (process.env.NODE_ENV === 'development') {
-Replacement).
+if (process.env.NODE_ENV === 'development') 
+{
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
